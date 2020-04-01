80ebbaac2514e67df70e4dc0cc1a2b5b77bc403c
Daniel Sellergren <danielsellergren@gmail.com>
2020-03-31 21:41:38 -0400

    Adds punctuation

--- merge_sort_verbose.rb
+++ merge_sort_verbose.rb
@@ -1,16 +1,16 @@
 # Merge Sort
 #
-# Input: A sequence of numbers in any order
-# Output: An ordered sequence from smallest number to largest
+# Input: A sequence of numbers in any order.
+# Output: An ordered sequence from smallest number to largest.
 #
 def merge_sort(numbers)
 
-  # Empty sets and singletons are already sorted
+  # Empty sets and singletons are already sorted.
   if numbers.length <= 1
     return numbers
   end
 
-  # Split array in two
+  # Split array in two.
   middle = numbers.length / 2
   left = numbers[0...middle]
   right = numbers[middle..numbers.length-1]
@@ -18,35 +18,35 @@ def merge_sort(numbers)
   puts left.inspect
   puts right.inspect
 
-  # Recursively call merge_sort to divide
-  # array into continually smaller sets
+  # Recursively call merge_sort to divide.
+  # array into continually smaller sets.
   left = merge_sort(left)
   right = merge_sort(right)
 
   puts left.inspect
   puts right.inspect
 
-  # Merge sets
+  # Merge sets.
   merge(left, right)
 end
 
 def merge(left, right)
   result = []
 
-  # As long as there are elements to compare
+  # As long as there are elements to compare.
   while left.length > 0 || right.length > 0
 
-    # If there are elements in both sets to compare
+    # If there are elements in both sets to compare.
     if left.length > 0 && right.length > 0
 
-      # Smallest element is added to result
+      # Smallest element is added to result.
       if left[0] <= right[0]
         result << left.shift
       else
         result << right.shift
       end
 
-    # Otherwise, add remaining elements
+    # Otherwise, add remaining elements.
     elsif left.length > 0
       result << left.shift
     elsif right.length > 0
@@ -57,10 +57,10 @@ def merge(left, right)
   result
 end
 
-# Generate array of ten random integers
+# Generate array of ten random integers.
 unsorted = (0..7).to_a.sort{ rand() - 0.5 }[0..7]
 puts unsorted.inspect
 
-# Sort
+# Sort.
 sorted = merge_sort(unsorted)
 puts sorted.inspect
